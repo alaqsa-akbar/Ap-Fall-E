b3130a23b419ab84b6d5abb83616a9df635c309f
ZezoTheBest <ziadtareq1123@gmail.com>
2023-04-11 21:39:03 +0300

    Added the media module and changed some things

--- src/main/java/com/example/ics108_project/GameClass.java
+++ src/main/java/com/example/ics108_project/GameClass.java
@@ -1,35 +1,60 @@
 package com.example.ics108_project;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class GameClass extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException,URISyntaxException {
         //Final Part (At the end)
+        Scene menuScene = mainMenuScene();
+        Scene gameScene = gameScene();
 
+        stage.setScene(menuScene);
+
+        //Decide the name of the game??
+        stage.setTitle("Ap-FALL-E");
+
+        //Set the icon of the game
+        Image iconImage = new Image("com/example/ics108_project/AppleLogo.PNG");
+        stage.getIcons().add(iconImage);
+
+        //Show the stage
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
 
-    private static Scene mainMenuScene()
+
+    private static Scene mainMenuScene() throws URISyntaxException
     {
         //Ziad's Part
         //Create Scene and return it to the start function
         Pane mainPane = new Pane();
+        File file = new File("src\\main\\resources\\PinkPanther.mp3");
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+
         Scene menuScene = new Scene(mainPane);
 
         return menuScene;
 
     }
+
+
     private static Scene gameScene()
     {
         //Al Aqsa's Part
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ics108_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.ics108_project to javafx.fxml;
